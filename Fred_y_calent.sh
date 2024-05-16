0693f6c979ff2cc19fc656aa787feb021c9967b0
Your Name <you@example.com>
2024-05-16 09:09:34 +0200

    Fred,calent_y_eureka

---
+++ Fred_y_calent.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Genera un nombre aleatori entre 1 i 10:
+adivina=$(( ( RANDOM % 10 )  + 1 ))
+
+########################################################
+# Escriu el teu codi aqui
+########################################################
+echo "Tres intents per adivinar el numero entre el 1 i el 10"
+
+# Inicialitzar el nombre d'intents
+intents=3
+
+while [ $intents -gt 0 ]; do
+    read -p "Introdueix un nombre: " NUMERO
+    if [ $NUMERO -eq $adivina ]; then
+        echo "Eureka"
+        exit
+    elif [ $NUMERO -lt $adivina ]; then
+        echo "FRED"
+    else
+        echo "CALENT"
+    fi
+    $intents=$(( ${intents} - 1 )) # Corregir la disminució dels intents
+done
+
+echo "Has acabat amb els tres intents. El nombre a adivinar era: ${adivina}"
+
+echo "El nombre que havies d'adivinar era: ${adivina}"
+########################################################
+# Fi del teu codi
+########################################################
